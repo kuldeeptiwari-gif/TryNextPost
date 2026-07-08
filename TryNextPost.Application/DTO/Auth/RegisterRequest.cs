eb0cf5d13f225be847a8df8de0488d6de00b9b65
kuldeeptiwari-gif <kuldeep.tiwari@luminnoverse.com>
2026-07-08 11:07:57 +0530

    Commit the code

--- TryNextPost.Application/DTO/Auth/RegisterRequest.cs
+++ TryNextPost.Application/DTO/Auth/RegisterRequest.cs
@@ -13,19 +13,21 @@ namespace TryNextPost.Application.DTO.Auth
         [EmailAddress(ErrorMessage = "Invalid email format")]
         public string Email { get; set; }
 
-        [Required(ErrorMessage = "Password is required")]
-        [MinLength(6, ErrorMessage = "Password must be at least 6 characters")]
-        public string Password { get; set; }
-
-        [Required(ErrorMessage = "Confirm Password is required")]
-        [Compare("Password", ErrorMessage = "Password and Confirm Password do not match")]
-        public string ConfirmPassword { get; set; }
-
-        [Required(ErrorMessage = "Full Name is required")]
-        public string FullName { get; set; }
+        [Required(ErrorMessage = "First Name is required")]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
 
         [Required(ErrorMessage = "Mobile number is required")]
         [Phone(ErrorMessage = "Invalid mobile number format")]
         public string Mobile { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [MinLength(6, ErrorMessage = "Password must be at least 6 characters")]
+        public string Password { get; set; }
+
+        //[Required(ErrorMessage = "Confirm Password is required")]
+        //[Compare("Password", ErrorMessage = "Password and Confirm Password do not match")]
+        //public string ConfirmPassword { get; set; }
+
     }
 }
